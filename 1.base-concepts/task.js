f42d49c71c54b3e7e3faab7589c3811b51c6e594
NataliaDomanova <n.domanova@mail.ru>
2022-06-13 03:45:14 +0300

    task.js changed

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,8 +1,34 @@
+"use strict";
+
 function solveEquation(a, b, c) {
-  let arr;
   // код для задачи №1 писать здесь
-  return arr; // array
+  let x1;
+  let x2; 
+  let arr = [x1, x2];
+  
+  let d = b**2-4*a*c;
+  
+  
+  if(d < 0) {  
+    return arr = [];
+  }
+  
+  else if(d === 0) {
+    x1 = -b/(2*a);
+    return arr = [x1];
+  }
+    
+  else if(d > 0){
+    x1 = (-b + Math.sqrt(d))/(2*a);
+    x2 = (-b - Math.sqrt(d))/(2*a);
+    return arr = [+x1.toFixed(3), +x2.toFixed(3)]
+  }  
 }
+
+solveEquation(a, b, c)
+
+
+
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
