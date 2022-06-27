5cdf26a13b6c602460049730984feba03391ef16
NataliaDomanova <n.domanova@mail.ru>
2022-06-28 01:52:19 +0300

    changed task.js

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -11,8 +11,7 @@ function advancedFilter(arr) {
   let resultArr = arr
     .filter((item) => item > 0) 
     .filter((item) => item % 3 === 0)
-    .map((item) => item * 10)       
-  // Ваш код
-
+    .map((item) => item * 10)  
+         
   return resultArr; // array
 }
