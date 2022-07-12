fd6f862ada46095b6bec82e4af69f48a43e457e9
NataliaDomanova <n.domanova@mail.ru>
2022-07-12 16:55:58 +0300

    Changed task.js

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -66,17 +66,72 @@ class PrintEditionItem {
     }
   }
   
-  const sherlock = new PrintEditionItem("Полное собрание повестей и рассказов о Шерлоке Холмсе в одном томе", 2019, 1008);
+  class Library {
+    constructor(name) {
+      this.name = name;
+      this.books = [];
+    }
+    addBook(book) {
+      if (book.state > 30) {
+        this.books.push(book)
+        return this.books.name
+      }
+    }
   
+    findBookBy(type, value) {
+      let book;
+      if (book = this.books.find(item => item[type] == value))
+        return book;
+      else return null;
+    }
+  
+    
+    giveBookByName(bookName) {
+      let foundBook;
+      let foundBookId;      
+      if (foundBook = this.books.find(item => item.name == bookName)) {
+        foundBookId = this.books.indexOf(foundBook);
+        this.books.splice(foundBookId, 1);
+        return foundBook;
+      }  
+      return null
+    }
+    
+    /*giveBookByName(bookName) {
+      for (let i = 0; i < this.books.length; i++) {
+        if (this.books[i].name == bookName) {
+          let foundBook = this.books[i]
+          this.books.splice([i], 1);
+          return foundBook;
+        }
+      }
+      return null;
+    }*/
+  }
+  
+  
+  const library = new Library("Библиотека имени Ленина");
   const picknick = new FantasticBook("Аркадий и Борис Стругацкие", "Пикник на обочине", 1972, 168);
+  const sherlock = new DetectiveBook("Артур Конан Дойл", "Полное собрание повестей и рассказов о Шерлоке Холмсе в одном томе", 2019, 10080);
+  const machine = new NovelBook("Герберт Уэллс", "Машина времени", 1895, 138);
+  const murzilka = new Magazine("Мурзилка", 1924, 60);
+  const markes = new NovelBook("Габриэль Гарсиа Маркес", "Сто лет одиночества", 2016, 544);
+  picknick.state = 80;
+  sherlock.state = 45;
+  machine.state = 65;
+  murzilka.state = 150;
+  markes.state = 25;
+  library.addBook(picknick);
+  library.addBook(sherlock);
+  library.addBook(machine);
+  library.addBook(murzilka);
+  library.addBook(new NovelBook("Габриэль Гарсиа Маркес", "Сто лет одиночества", 2016, 544));
   
-  console.log(sherlock.releaseDate); //2019
-  console.log(sherlock.state); //100
-  sherlock.fix();
-  console.log(sherlock.state); //100
   
-  console.log(picknick.author); //"Аркадий и Борис Стругацкие"
-  picknick.state = 10;
-  console.log(picknick.state); //10
-  picknick.fix();
-  console.log(picknick.state); //15
+  library.giveBookByName('Сто лет одиночества');
+  console.log(library.books);
+  markes.state = 25;
+  markes.fix();
+  library.addBook(markes);
+  console.log(library.books);
+  
