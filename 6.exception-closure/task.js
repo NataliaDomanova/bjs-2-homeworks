de5b285b80d911154c332b9ecf41d86c9cf1b4b7
NataliaDomanova <n.domanova@mail.ru>
2022-07-17 17:28:26 +0300

    new changes task.js

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -9,17 +9,49 @@ function parseCount(x) {
 
 function validateCount(x) {
   try {
-    function parseCount(x) {
-      let parsed = Number.parseInt(x);
-      if (isNaN(parsed))
-        throw new Error("Невалидное значение");
-      else
-        return parsed;
-    }
-    return parseCount(x);
+    const newCount = parseCount(x);
+    return newCount;
+  } catch (error) {
+    return error;
   }
-  catch (err) {
-    return err;
+  return newCount;
+}
+
+
+console.log(validateCount('nhg123'));
+//////////////
+
+class Triangle {
+  constructor(a, b, c) {
+    this.a = a;
+    this.b = b;
+    this.c = c;
+    if (a >= b + c || b >= a + c || c >= a + b) {
+      throw new Error("Треугольник с такими сторонами не существует");
+    }
+  }
+
+  getPerimeter() {
+    let perimeter = this.a + this.b + this.c;
+    return perimeter;
+  }
+
+  getArea() {
+    let p = (this.a + this.b + this.c) / 2;
+    let s = +(Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c))).toFixed(3);
+   return (s);
   }
 }
-console.log(validateCount('nhg123'));
+
+function getTriangle(a, b, c) {
+  let triangle = new Triangle(a, b, c);
+   return triangle;
+ }
+
+console.log(getTriangle(12, 20, 30));
+const one = new Triangle(4, 5, 6);
+const two = new Triangle(45, 25, 55);
+const three = new Triangle(100, 25, 45);
+console.log(one);
+console.log(two);
+
