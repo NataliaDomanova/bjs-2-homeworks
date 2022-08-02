90b590c3444a615c77e92cc7c22808f53095a0bc
NataliaDomanova <n.domanova@mail.ru>
2022-08-02 03:00:10 +0300

    Changed task.js

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,6 +1,39 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let cache = [];
+
+  function wrapper(...args) {
+    const hash = args.join('');
+    let result = func(...args); 
+    let objectInCache = cache.find((item) => item.hash === hash);
+
+    if (objectInCache !== undefined) { // если элемент найден
+      console.log("Из кэша: " + objectInCache.result); // индекс нам известен, по индексу в массиве лежит объект, как получить нужное значение?
+      return "Из кэша: " + objectInCache.result;
+    }
+    cache.push({ hash, result }); 
+     //console.log(cache)
+    if (cache.length > 5) {
+      cache.shift();
+    }
+    console.log("Вычисляем: " + result);
+    return "Вычисляем: " + result;
+  }
+   //console.log(cache)
+  return wrapper;
+ 
 }
+
+
+const addThree = (a, b, c) => a + b + c;
+const upgradedAddThree = cachingDecoratorNew(addThree);
+upgradedAddThree(1, 2, 3); // вычисляем: 6
+upgradedAddThree(1, 2, 3); // из кэша: 6
+upgradedAddThree(2, 2, 3); // вычисляем: 7
+upgradedAddThree(3, 2, 3); // вычисляем: 8
+upgradedAddThree(4, 2, 3); // вычисляем: 9
+upgradedAddThree(5, 2, 3); // вычисляем: 10
+upgradedAddThree(6, 2, 3); // вычисляем: 11   (при этом кэш для 1, 2, 3 уничтожается)
+upgradedAddThree(1, 2, 3); // вычисляем: 6  (снова вычисляем, кэша нет)
 
 
 function debounceDecoratorNew(func) {
