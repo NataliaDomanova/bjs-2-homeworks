07bf02f69299f314013c716d9e3d55ef80d6afa7
NataliaDomanova <n.domanova@mail.ru>
2022-08-04 00:53:53 +0300

    Changes

--- 7.async/task.js
+++ 7.async/task.js
@@ -24,46 +24,101 @@ class AlarmClock {
       return true;
     } else return false;
   }
+  // Вариант №1 (параметр min < 60 минут)
+  
+    getCurrentFormattedTime(min = 0) {
+      const currentDate = new Date();
+      let hours = currentDate.getHours();
+      let minutes = currentDate.getMinutes() + min;
 
-  getCurrentFormattedTime() {
+      if (min > 59) {
+      console.error('Параметр min превышает 59 минут');
+      return;
+      }
+      if (minutes > 59) {      
+        minutes = minutes - 60;
+        hours = hours + 1;
+      }
+      if (hours > 23) {
+        hours = '00';
+      }
+      if (hours < 10) {
+        hours = '0' + hours;
+      }
+      if (minutes < 10) {
+        minutes = '0' + minutes;
+      }
+      let time = hours + ':' + minutes;
+  
+      //const time = (`${hours} : ${minutes}`);
+      return time;
+    }
+  
+  
+  //Вариант№2 (параметр min не больше 1440 минут = 1 сутки)
+  /*
+  getCurrentFormattedTime(min = 0) {
     const currentDate = new Date();
     let hours = currentDate.getHours();
+    let minutes = currentDate.getMinutes() + min;
+
+    if (min > 1440) {
+      throw new Error('Параметр min превышает 1440 минут');
+    };
+
+    if (minutes > 59) {
+      minutes = (minutes % 60);
+      hours = hours + (+(min / 60).toFixed(0));
+    }
+    if (hours === 24) {
+      hours = '00';
+    }
+
+    if (hours > 24) {
+      hours = hours - 24;
+    }
+
+    if (minutes < 10) {
+      minutes = '0' + minutes;
+    }
     if (hours < 10) {
       hours = '0' + hours;
     }
 
-    let minutes = currentDate.getMinutes();
-    if (minutes < 10) {
-      minutes = '0' + minutes;
-    }
     let time = hours + ':' + minutes;
-
     //const time = (`${hours} : ${minutes}`);
     return time;
   }
-/*  //вариант1
-  start() {
+  
+   */
+
+  //Вариант 1 start
+  /*  
+    start() {
+      let checkClock = (call) => {
+        if (this.getCurrentFormattedTime() === call.time)
+          return call.callback();
+      }
+  
+      let forEachCall = () => {
+        this.alarmCollection.forEach(checkClock);
+      }
+      this.timerId = setInterval(forEachCall, 1000);
+    }
+
+  */
+  //Вариант 2 start
+  start() { 
     let checkClock = (call) => {
       if (this.getCurrentFormattedTime() === call.time)
         return call.callback();
     }
 
-    let forEachCall = () => {
-      this.alarmCollection.forEach(checkClock);
+    if (this.timerId === null) {
+      this.timerId = setInterval(() => this.alarmCollection.forEach(checkClock), 2000);
     }
-    this.timerId = setInterval(forEachCall, 1000);
   }
-*/
-  //вариант 2
-    start() {
-      let checkClock = (call) => {
-        if (this.getCurrentFormattedTime() === call.time)
-         return call.callback();
-      }
-  
-      this.timerId = setInterval(() => this.alarmCollection.forEach(checkClock), 1000);
-    
-  }
+
   stop() {
     if (this.timerId) {
       clearInterval(this.timerId);
@@ -84,22 +139,25 @@ class AlarmClock {
 
 function testCase() {
 
-
   let phoneAlarm = new AlarmClock;
   phoneAlarm.addClock(phoneAlarm.getCurrentFormattedTime(), () => console.log('Пора вставать'), 1);
-  phoneAlarm.addClock(phoneAlarm.getCurrentFormattedTime(), () => { console.log('Давай вставай уже!'); phoneAlarm.removeClock(2) }, 2);
+  phoneAlarm.addClock(phoneAlarm.getCurrentFormattedTime(1), () => { console.log('Давай вставай уже!'); 
+  phoneAlarm.removeClock(2) }, 2);
   //phoneAlarm.addClock('09:01', () => console.log('Иди умываться'));
-  phoneAlarm.addClock(phoneAlarm.getCurrentFormattedTime(), () => {
-    console.log('Вставай, а то проспишь');
-    phoneAlarm.clearAlarms();
-    phoneAlarm.printAlarms();
+  phoneAlarm.addClock(phoneAlarm.getCurrentFormattedTime(58), () => {
+  console.log('Вставай, а то проспишь');
+  phoneAlarm.clearAlarms();
+  phoneAlarm.printAlarms();
   }, 3);
+
   //phoneAlarm.addClock('09:05', 'Вставай, а то проспишь', 1);
   //console.log(phoneAlarm.alarmCollection);
   //console.log(phoneAlarm.alarmCollection[0].time);
   phoneAlarm.printAlarms();
   phoneAlarm.start();
+  //console.log(phoneAlarm.alarmCollection);
   //phoneAlarm.stop();
+
 }
 
 testCase();
